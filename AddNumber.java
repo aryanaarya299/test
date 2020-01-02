976cca5794344ab3c4ce1c8fcea2451dd0fba8ac
aryanaarya299 <anasjohn299@gmail.com>
2020-01-02 19:17:39 +0530

    First code Addnumber

---
+++ AddNumber.java
@@ -0,0 +1,17 @@
+package revisionjava;
+
+public class AddNumber {
+
+	public static void main(String[] args) {
+      System.out.println(" I Lubg u Pooja");
+		
+		
+		int x =10;
+		int y = 20;
+		int result = x + y;
+		
+		System.out.println(" result Fat " + result);
+		
+	}
+
+}
